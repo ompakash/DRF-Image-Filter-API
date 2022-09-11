de6ccefd060389265e7f09468f6a689307273ead
omprakash <opytc2@gmail.com>
2022-09-11 18:06:31 +0530

    Scalling the process

--- api/img_converter.py
+++ api/img_converter.py
@@ -6,6 +6,11 @@ base_dir = settings.BASE_DIR
 
 class ImgConverter:
 
+    def __init__(self, process, data):
+        self.name = data["name"]
+        self.process = process
+        self.image = data["before_img"]
+
     def gray_scale(self, img):
         new_img = img[1:]
         file_address = os.path.join(base_dir, new_img)
@@ -19,3 +24,14 @@ class ImgConverter:
         # saving structure
         cv2.imwrite(after_images, read_img)
         return return_address
+
+    def QRfunc(self, img):
+        return f"{img}: img url"
+
+
+    def callProcess(self):
+        if self.process == "bnw":
+            return self.gray_scale(self.image)
+
+        elif self.process == "qr":
+            return self.QRfunc(self.image)
--- api/views.py
+++ api/views.py
@@ -2,27 +2,32 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
+
 from .serializers import ImageFilterSerializer
 from .models import ImageFilter
 from api.img_converter import ImgConverter
-
-ImgConverter_obj = ImgConverter()
+from rest_framework.permissions import IsAuthenticated
 
 class ImageFilterViews(APIView):
-    def get(self,request,name = None):
+    # permission_classes = [IsAuthenticated]
+
+    def get(self, request, name=None):
 
         all_img = ImageFilter.objects.all()
-        serializer = ImageFilterSerializer(all_img,many=True)
+        serializer = ImageFilterSerializer(all_img, many=True)
 
-        return Response({'msg':'all images','status':status.HTTP_200_OK,'data':serializer.data})
-        
+        return Response({'msg': 'all images', 'status': status.HTTP_200_OK, 'data': serializer.data})
 
-    def post(self,request):
-        serializer = ImageFilterSerializer(data = request.data)
+    def post(self, request):
+        serializer = ImageFilterSerializer(data=request.data)
 
         if serializer.is_valid():
             serializer.save()
-            output_address = ImgConverter_obj.gray_scale(serializer.data['before_img'])
-            return Response({'msg':'Done','status':status.HTTP_201_CREATED,'payload':serializer.data,'after_process': output_address})
 
-        return Response({'msg':'Something went wrong ','errors':serializer.errors,'status':status.HTTP_400_BAD_REQUEST})
+            process = request.data["process"]
+            ImgConverter_obj = ImgConverter(process, serializer.data)
+            output_address = ImgConverter_obj.callProcess()
+                
+            return Response({'msg': 'Done', 'status': status.HTTP_201_CREATED, 'payload': serializer.data, 'after_process': output_address})
+
+        return Response({'msg': 'Something went wrong ', 'errors': "serializer.errors", 'status': status.HTTP_400_BAD_REQUEST})
